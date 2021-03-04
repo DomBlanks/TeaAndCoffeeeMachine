1e4fdd2128a5dd86bb8936e038f9cb5f9dd3e8a7
Dominic Blanks <dominic.blanks@parkcameras.com>
2021-03-04 16:26:12 +0000

    Add project files.

---
+++ TeaAndCoffeeeMachine/Controllers/DrinksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TeaAndCoffeeeMachine.Controllers
+{
+    [Route("[controller]")]
+    public class DrinksController : Controller
+    {
+        // GET: /<controller>/
+        [Route("[action]")]
+        public IActionResult LemonTea()
+        {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult Coffee()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult Chocolate()
+        {
+            return View();
+        }
+
+    }
+}
